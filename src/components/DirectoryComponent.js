42b6853768333ce8a93df64e6d4f8cc935fbeeaa
Bryan Welch <welchbs44@gmail.com>
2021-07-25 09:42:17 -0400

    crested campsiteinfocomponent.js

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,7 +1,7 @@
 import { div } from 'prelude-ls';
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-import {} from './CampsiteInfoComponent'
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import CampsiteInfo from './CampsiteInfoComponent'
 
 class Directory extends Component {
     constructor(props) {
@@ -13,21 +13,6 @@ class Directory extends Component {
 
     onCampsiteSelect(campsite) {
         this.setState({selectedCampsite: campsite});
-    }
-
-    renderSelectedCampsite(campsite) {
-        if (campsite) {
-            return (
-                <Card>
-                    <CardImg top src={campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
-            )
-        }
-        return <div />;
     }
 
     render() {
@@ -49,11 +34,7 @@ class Directory extends Component {
                 <div className="row">
                     {directory}
                 </div>
-                <div calssName="row">
-                    <div className="col-md-5 m-1">
-                        {this.renderSelectedCampsite(this.state.selectedCampsite)}
-                    </div>
-                </div>
+                <CampsiteInfo campsite = {this.state.selectedCampsite}/>
             </div>
         );
     }
